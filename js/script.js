d3059b8fcdc29cd34865f44569730fb9af68294d
enzoarce <enzoarcero@gmail.com>
2023-05-07 01:56:12 -0300

    agregando la funcionalidad de encriptar un texto

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+    
+    if (texto.value.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/encriptado.jpg";
+    } else {
+        muñeco.src = "./img/persona.svg";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingrese el texto que desee encriptar o desencriptar";
+        alert("Debe ingresar algún texto");
+    }
+}
